367a2ba0e9c805a3b84dc08b1f9458305f2116a9
denhur62 <denhur62@naver.com>
2021-06-28 14:28:47 +0900

    integerEncoding

---
+++ integerEncoding.py
@@ -0,0 +1,36 @@
+from collections import Counter
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+text = "A barber is a person. a barber is good person. a barber is huge person. he Knew A Secret! The Secret He Kept is huge secret. Huge secret. His barber kept his word. a barber kept his word. His barber kept his secret. But keeping and keeping such a huge secret to himself was driving the barber crazy. the barber went up a huge mountain."
+text = sent_tokenize(text)
+# 정제와 단어 토큰화
+vocab = {} # 파이썬의 dictionary 자료형
+sentences = []
+stop_words = set(stopwords.words('english'))
+
+for i in text:
+    sentence = word_tokenize(i) # 단어 토큰화를 수행합니다.
+    result = []
+
+    for word in sentence: 
+        word = word.lower() # 모든 단어를 소문자화하여 단어의 개수를 줄입니다.
+        if word not in stop_words: # 단어 토큰화 된 결과에 대해서 불용어를 제거합니다.
+            if len(word) > 2: # 단어 길이가 2이하인 경우에 대하여 추가로 단어를 제거합니다.
+                result.append(word)
+                if word not in vocab:
+                    vocab[word] = 0 
+                vocab[word] += 1
+    sentences.append(result) 
+
+sentences = sum(sentences,[])
+vocab= Counter(sentences)
+vocab_size = 5
+vocab = vocab.most_common(vocab_size) 
+word_to_index = {}
+i = 0
+for (word, frequency) in vocab :
+    i = i+1
+    word_to_index[word] = i
+print(word_to_index)
