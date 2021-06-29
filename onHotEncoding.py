c9dcc2a6a38b9dd46cd08667a7d12ab44312944e
denhur62 <denhur62@naver.com>
2021-06-29 14:18:25 +0900

    add requrements.txt

---
+++ onHotEncoding.py
@@ -0,0 +1,8 @@
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.utils import to_categorical
+
+text="나랑 점심 먹으러 갈래 점심 메뉴는 햄버거 갈래 갈래 햄버거 최고야"
+
+t=Tokenizer()
+encoded=t.texts_to_sequences([text])[0]
+print(encoded)
