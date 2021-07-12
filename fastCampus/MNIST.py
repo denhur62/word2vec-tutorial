5c9553fbe462ada7a43192cc4006283eb41d0c36
denhur62 <denhur62@naver.com>
2021-07-12 21:09:26 +0900

    modifyied structure model
    modifyied structure model

--- fastCampus/MNIST.py
+++ fastCampus/MNIST.py
@@ -51,22 +51,85 @@ y = list(y.split(cnts, dim=0))
 x += [(test.data.float() / 255.).view(test_cnt, -1)]
 y += [test.targets]
 
-model = nn.Sequential(
-    nn.Linear(input_size, 500),
-    nn.LeakyReLU(),
-    nn.Linear(500, 400),
-    nn.LeakyReLU(),
-    nn.Linear(400, 300),
-    nn.LeakyReLU(),
-    nn.Linear(300, 200),
-    nn.LeakyReLU(),
-    nn.Linear(200, 100),
-    nn.LeakyReLU(),
-    nn.Linear(100, 50),
-    nn.LeakyReLU(),
-    nn.Linear(50, output_size),
-    nn.LogSoftmax(dim=-1),
-)
+
+class Block(nn.Module):
+
+    def __init__(self,
+                 input_size,
+                 output_size,
+                 use_batch_norm=True,
+                 dropout_p=.4):
+        self.input_size = input_size
+        self.output_size = output_size
+        self.use_batch_norm = use_batch_norm
+        self.dropout_p = dropout_p
+
+        super().__init__()
+
+        def get_regularizer(use_batch_norm, size):
+            return nn.BatchNorm1d(size) if use_batch_norm else nn.Dropout(dropout_p)
+
+        self.block = nn.Sequential(
+            nn.Linear(input_size, output_size),
+            nn.LeakyReLU(),
+            get_regularizer(use_batch_norm, output_size),
+        )
+
+    def forward(self, x):
+        # |x| = (batch_size, input_size)
+        y = self.block(x)
+        # |y| = (batch_size, output_size)
+
+        return y
+
+
+class MyModel(nn.Module):
+
+    def __init__(self,
+                 input_size,
+                 output_size,
+                 use_batch_norm=True,
+                 dropout_p=.4):
+
+        super().__init__()
+
+        self.layers = nn.Sequential(
+            Block(input_size, 500, use_batch_norm, dropout_p),
+            Block(500, 400, use_batch_norm, dropout_p),
+            Block(400, 300, use_batch_norm, dropout_p),
+            Block(300, 200, use_batch_norm, dropout_p),
+            Block(200, 100, use_batch_norm, dropout_p),
+            nn.Linear(100, output_size),
+            nn.LogSoftmax(dim=-1),
+        )
+
+    def forward(self, x):
+        # |x| = (batch_size, input_size)
+        y = self.layers(x)
+        # |y| = (batch_size, output_size)
+
+        return y
+
+
+model = MyModel(input_size,
+                output_size,
+                use_batch_norm=True)
+# model = nn.Sequential(
+#     nn.Linear(input_size, 500),
+#     nn.LeakyReLU(),
+#     nn.Linear(500, 400),
+#     nn.LeakyReLU(),
+#     nn.Linear(400, 300),
+#     nn.LeakyReLU(),
+#     nn.Linear(300, 200),
+#     nn.LeakyReLU(),
+#     nn.Linear(200, 100),
+#     nn.LeakyReLU(),
+#     nn.Linear(100, 50),
+#     nn.LeakyReLU(),
+#     nn.Linear(50, output_size),
+#     nn.LogSoftmax(dim=-1),
+# )
 
 crit = nn.NLLLoss()
 
